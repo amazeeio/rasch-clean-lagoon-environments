3edbb578129244d163711270ca498d7a9112a4ee
Blaize Kaye <blaize.kaye@gmail.com>
2023-02-13 09:20:21 +1300

    Update runcleanup.sh

--- runcleanup.sh
+++ runcleanup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-ssh -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 32222 -t lagoon@ssh.lagoon.amazeeio.cloud token
-#LAGOON_API_TOKEN=$(ssh -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 32222 -t lagoon@ssh.lagoon.amazeeio.cloud token 2> /dev/null | tr -d '\n\r')
-#LAGOON_API_TOKEN=$LAGOON_API_TOKEN GITHUB_API_TOKEN=$GITHUB_API_TOKEN ./lagoon-envs-cleanup.sh -g rasch -p $PROJECTS
+LAGOON_API_TOKEN=$(ssh -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 32222 -t lagoon@ssh.lagoon.amazeeio.cloud token 2> /dev/null | tr -d '\n\r')
+LAGOON_API_TOKEN=$LAGOON_API_TOKEN GITHUB_API_TOKEN=$GITHUB_API_TOKEN ./lagoon-envs-cleanup.sh -g rasch -p $PROJECTS
